f8e7e28db4b664183113767fd0b97edccd6edd6e
Archishmaan Peyyety <archishou@gmail.com>
2020-03-13 17:11:33 -0400

    component sub folder
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Clock from './Components/Field.js'
 import './App.css';
 
 function App() {
   return (
       <div>
       <Clock />
+      <Toggle />
       </div>
   );
 }
 
-class Clock extends React.Component {
+
+class Toggle extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {date: new Date()};
+        this.state = {isToggleOn: true};
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        });
+    handleClick() {
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }));
     }
 
     render() {
         return (
-            <div>
-                <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
         );
     }
 }
+
 export default App;
