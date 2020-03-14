7eec4722af3ba4db327a7805500c6a203a362615
Archishmaan Peyyety <archishou@gmail.com>
2020-03-13 20:53:23 -0400

    yay its rlly good
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>

--- src/Components/Field/Field.js
+++ src/Components/Field/Field.js
@@ -1,22 +1,52 @@
-import React, { Component } from "react";
+import React from 'react';
 import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
-import field from "./resources/skystone_field.jpg";
+import './Field.css'
+import field_img from './resources/skystone_field.jpg'
 
-export default class Field extends Component {
-    fieldDivStyle = {
-        border: '1px solid red',
+
+export default class Field extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            rotation: 0
+        };
+
+        this.rotate = this.rotate.bind(this);
+    }
+
+    fieldViewPort = {
+        width: '100vh',
+        height: '100vh',
+        border: '3px solid black',
         float: 'left',
-        width : '50%',
+        backgroundColor: 'black',
+    };
+    fieldDivStyle = {
+        width: '100vh',
+        height: '100vh',
+        float: 'left'
     };
 
+    initScale = 0.4;
+
+    rotate(){
+        let newRotation = this.state.rotation + 90;
+        if(newRotation >= 360){
+            newRotation =- 360;
+        }
+        this.setState({
+            rotation: newRotation,
+        })
+    }
+
     render() {
+        const { rotation } =  this.state;
         return (
-            <div
-                style={this.fieldDivStyle}>
+            <div style={this.fieldViewPort}>
                 <TransformWrapper
-                    defaultScale={1}
-                    defaultPositionX={0}
-                    defaultPositionY={0}
+                    defaultScale={this.initScale}
+                    defaultPositionX={500}
+                    defaultPositionY={500}
 
                     pan={{
                         velocity: false
@@ -24,11 +54,14 @@ export default class Field extends Component {
                     options={{
                         limitToWrapper: false,
                         limitToBounds: false,
+                        minScale: this.initScale,
+                        maxScale: 10,
                     }}
-
                 >
                     <TransformComponent>
-                        <img src={field} />
+                        <div style={this.fieldDivStyle}>
+                            <img src={field_img} onClick={this.rotate}/>
+                        </div>
                     </TransformComponent>
                 </TransformWrapper>
             </div>
