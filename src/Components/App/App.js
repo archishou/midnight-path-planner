3b584a7c61b2e33a46838e70b29075a01c2f7649
Archishmaan Peyyety <archishou@gmail.com>
2020-03-16 14:50:53 -0400

    what is going on
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,44 +1,20 @@
 import React from 'react';
 import Field from '../Field/Field.js'
-import fieldViewHeight from '../Field/Field.js'
-import fieldViewWidth from '../Field/Field.js'
 import './App.css';
 
 const fieldViewPort = {
-    width: fieldViewWidth,
-    height: fieldViewHeight,
-    border: '3px solid black',
+    width: '50%',
+    height: '100%',
+    border: '10px solid red',
     float: 'left',
     backgroundColor: 'black',
 };
 
-const buttonStyle = {
-    background: '#4CAF50', /* Green */
-    border: 'none',
-    color: 'white',
-    padding: '15px 32px'
-};
-
-const consoleViewPort = {
-    border: '3px solid black',
-    float: 'right',
-    backgroundColor: 'black',
-};
-
-const divButtonStyle = {
-    margin: 'auto'
-};
 function App() {
 
   return (
-      <div>
-          <div style={fieldViewPort}>
-              <Field />
-              <div style={divButtonStyle}>
-                  <button style={buttonStyle}>Test</button>
-              </div>
-
-          </div>
+      <div style={fieldViewPort}>
+          <Field />
       </div>
   );
 }
--- src/Components/Field/Field.js
+++ src/Components/Field/Field.js
@@ -3,59 +3,18 @@ import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 import './Field.css'
 import field_img from './resources/skystone_field.jpg'
 import Immutable from "immutable";
-const fieldViewHeight = '90vh';
-const fieldViewWidth = '50vw';
 
-const DrawingLine = (props) => {
-    const pathData = "M " + props.line
-        .map(p => {
-            return `${p.get('x')} ${p.get('y')}`
-        })
-        .join(" L ");
-    console.log("in DrawingLine", pathData);
-    return <path className="path" d={pathData} />;
-};
 
 export default class Field extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isDrawing: false,
-            lines: new Immutable.List()
-        };
-        this.handleMouseDown = this.handleMouseDown.bind(this);
-    }
-
-    state = {
-        isDrawing: false,
-        lines: new Immutable.List()
-    };
-
     fieldDivStyle = {
-        width: fieldViewWidth,
-        height: fieldViewHeight,
-        float: 'left'
+        width: '50vw',
+        height: '90vh',
+        float: 'left',
+        border: '3px solid red',
     };
 
     initScale = 0.4;
-
-    relativeCoordinatesForEvent(event) {
-        return new Immutable.Map({
-            x: event.clientX ,
-            y: event.clientY ,
-        });
-    }
-    handleMouseDown = (event) => {
-        if (event.button !== 0) return;
-        const point = this.relativeCoordinatesForEvent(event);
-        this.setState(prevState => {
-            return {
-                lines: prevState.lines.push(new Immutable.List([point])),
-                isDrawing: true,
-            }
-        })
-    };
 
     render() {
         return (
@@ -75,16 +34,98 @@ export default class Field extends React.Component {
                 }}
             >
                 <TransformComponent>
-                    <div style={this.fieldDivStyle} onMouseDown={this.handleMouseDown}>
+                    <div style={this.fieldDivStyle} >
                         <img src={field_img}/>
-                        <svg className="drawing">
-                            {this.state.lines.map((line, index) => (
-                                <DrawingLine key={index} line={line} />
-                            ))}
-                        </svg>
                     </div>
                 </TransformComponent>
             </TransformWrapper>
         );
     }
 }
+
+/*
+ <Drawing lines={this.state.lines} />
+
+
+
+constructor(props) {
+    super(props);
+    this.state = {
+        lines: new Immutable.List(),
+        isDrawing: false
+    };
+
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+}
+
+initScale = 0.4;
+
+componentDidMount() {
+    document.addEventListener("mouseup", this.handleMouseUp);
+}
+
+componentWillUnmount() {
+    document.removeEventListener("mouseup", this.handleMouseUp);
+}
+
+handleMouseDown(mouseEvent) {
+    if (mouseEvent.button != 0) {
+        return;
+    }
+
+    const point = this.relativeCoordinatesForEvent(mouseEvent);
+
+    this.setState(prevState => ({
+        lines: prevState.lines.push(new Immutable.List([point])),
+        isDrawing: true
+    }));
+}
+
+handleMouseMove(mouseEvent) {
+    if (!this.state.isDrawing) {
+        return;
+    }
+
+    const point = this.relativeCoordinatesForEvent(mouseEvent);
+
+    this.setState(prevState =>  ({
+        lines: prevState.lines.updateIn([prevState.lines.size - 1], line => line.push(point))
+    }));
+}
+
+handleMouseUp() {
+    this.setState({ isDrawing: false });
+}
+
+relativeCoordinatesForEvent(mouseEvent) {
+    //const boundingRect = this.refs.drawArea.getBoundingClientRect();
+    return new Immutable.Map({
+        x: mouseEvent.clientX,
+        y: mouseEvent.clientY,
+    });
+}
+
+
+function Drawing({ lines }) {
+    return (
+        <svg className="drawing">
+            {lines.map((line, index) => (
+                <DrawingLine key={index} line={line} />
+            ))}
+        </svg>
+    );
+}
+
+function DrawingLine({ line }) {
+    const pathData = "M " +
+        line
+            .map(p => {
+                return `${p.get('x')} ${p.get('y')}`;
+            })
+            .join(" L ");
+
+    return <path className="path" d={pathData} />;
+}
+ */
