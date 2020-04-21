824fcb4c88955bab3b9345ba248a7a316ee49b22
Archishmaan Peyyety <archishou@gmail.com>
2020-04-21 15:32:39 -0400

    class
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,59 +1,67 @@
 import React from "react";
 import { Card, Form, Input, Button } from "antd";
 import routes from "../routes";
-const LoginPage = props => {
-  const { getFieldDecorator } = props.form;
-  const handleSubmit = () => {
+class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            emailValidation: "",
+            emailHelp: "",
+            passwordValidation: "",
+            passwordHelp: "",
+        }
+    }
+   handleSubmit = () => {
     // Set token into localstorage
     localStorage.setItem("token", "I am now logged in");
-    props.history.push(routes.field);
+    this.props.history.push(routes.field);
   };
-  return (
-    <>
-      <Card
-        bordered={false}
-        style={{
-          border: "1px solid #282828",
-          boxShadow: "0px 15px 20px 5px #0000001a",
-          width: 400,
-          backgroundColor: "grey",
-        }}
-      >
-        <h1 style={{ textAlign: "center" }}>Midnight Path Planning</h1>
-        <Form
-          hideRequiredMark
-          colon={false}
-          onSubmit={handleSubmit}
-          layout="vertical"
-        >
-          <Form.Item label="Email">
-            {getFieldDecorator("email", {
-              rules: [{ required: true, message: "Email required" }]
-            })(<Input size="large" placeholder="John@example.com" />)}
-          </Form.Item>
-          <Form.Item label="Password">
-            {getFieldDecorator("password", {
-              rules: [{ required: true, message: "Password required" }]
-            })(
-              <Input.Password
-                size="large"
-                type="password"
-                placeholder="Password"
-              />
-            )}
-          </Form.Item>
-          <Form.Item label="Robot Name">
-            {getFieldDecorator("robot_name", {
-              rules: [{ required: true, message: "Robot Name required" }]
-            })(<Input size="large" placeholder="Robot One" />)}
-            </Form.Item>
-          <Button block size="large" htmlType="submit">
-            Login
-          </Button>
-        </Form>
-      </Card>
-    </>
-  );
-};
+    handleChange = () => {
+
+    }
+   render() {
+       return (
+       <>
+           <Card
+               bordered={false}
+               style={{
+                   border: "1px solid #282828",
+                   boxShadow: "0px 15px 20px 5px #0000001a",
+                   width: 500,
+                   //backgroundColor: "grey",
+               }}
+           >
+               <h1 style={{ textAlign: "center" }}>Midnight Path Planning</h1>
+               <Form
+                   hideRequiredMark
+                   colon={false}
+                   onSubmit={this.handleSubmit}
+                   layout="vertical"
+                   onChange={this.handleChange}
+               >
+                   <Form.Item label="Email" rules={[{ required: true, message: "Email required" }]} validateStatus="success" help="">
+                       <Input size="large" placeholder="john@example.com" />
+                   </Form.Item>
+                   <Form.Item label="Password" rules={[{ required: true, message: "Password required" }]}>
+                       <Input.Password
+                           size="large"
+                           type="password"
+                           placeholder="Password"
+                       />
+                   </Form.Item>
+                   <Form.Item label="Robot Name">
+                       <Input size="large" placeholder="Robot One" />
+                   </Form.Item>
+                   <Form.Item>
+                       <Button block htmlType="submit" size="large">
+                           Login
+                       </Button>
+                   </Form.Item>
+               </Form>
+           </Card>
+       </>
+   );
+   }
+}
 
 export default Form.create({ name: "Login" })(LoginPage);
